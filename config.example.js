f4686cda408d86cb904ae2887ac253a511288523
reaper45 <jomwashighadi@gmail.com>
2018-02-09 07:58:49 +0300

    Removes keys

---
+++ config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+	consumer_key:         'consumer_key_here',
+	consumer_secret:      'consumer_secret_here',
+	access_token:         'access_token_here',
+	access_token_secret:  'access_token_secret_here',
+	timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
